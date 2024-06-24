1e67930c7a7ea926c85fed011880c0639da614c1
eladriere <enzo.ladriere.pro@gmail.com>
2024-06-24 14:15:12 +0200

    docker yaml pour platform

--- sql/postgresql.db.js
+++ sql/postgresql.db.js
@@ -2,12 +2,18 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 const { PostgresDialect } = require("@sequelize/postgres");
 
-exports.sequelize = new Sequelize({
-    dialect: 'postgres',
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.HOST,
-    port: process.env.DB_PORT,
-    clientMinMessages: 'notice',
-});
+let dsn = process.env.CONNECTION_STRING;
+if (dsn === undefined) {
+    const { env } = process;
+    const read_base64_json = function(varName) {
+        try {
+            return JSON.parse(Buffer.from(env[varName], "base64").toString())
+        } catch (err) {
+            throw new Error(`no ${varName} environment variable`)
+        }
+    };
+    const variables = read_base64_json('PLATFORM_VARIABLES')
+    dsn = variables["CONNECTION_STRING"]
+}
+
+exports.sequelize = new Sequelize(dsn);
